18dd29170b81a753bf672f764a19a96851cc8ed7
Suleiman Leadbitter <sulcalibur@me.com>
2023-03-15 18:40:23 +0000

    Commit from TeleportHQ

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -27,32 +27,6 @@ const Navigation = (props) => {
             {props.button}
           </a>
         </div>
-        <div data-role="BurgerMenu" className={styles['burger-menu']}>
-          <svg viewBox="0 0 1024 1024" className={styles['icon']}>
-            <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
-          </svg>
-        </div>
-        <div data-role="MobileMenu" className={projectStyles['mobile-menu']}>
-          <div className={styles['nav1']}>
-            <div className={styles['container']}>
-              <img alt={props.image_alt1} src={props.image_src1} />
-              <div
-                data-role="CloseMobileMenu"
-                className={styles['close-mobile-menu']}
-              >
-                <svg viewBox="0 0 1024 1024" className={styles['icon2']}>
-                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
-                </svg>
-              </div>
-            </div>
-            <NavigationLinks rootClassName="root-class-name18"></NavigationLinks>
-            <button
-              className={` ${projectStyles['button-secondary']} ${projectStyles['button']} ${projectStyles['button-md']} `}
-            >
-              {props.button2}
-            </button>
-          </div>
-        </div>
       </div>
     </header>
   )
@@ -61,18 +35,12 @@ const Navigation = (props) => {
 Navigation.defaultProps = {
   image_src: '/playground_assets/portfolio_title-200h.png',
   button: 'Get in touch',
-  image_alt1: 'image',
-  image_src1: '/playground_assets/logo1-1500h.png',
-  button2: 'Get in touch',
   image_alt: 'logo',
 }
 
 Navigation.propTypes = {
   image_src: PropTypes.string,
   button: PropTypes.string,
-  image_alt1: PropTypes.string,
-  image_src1: PropTypes.string,
-  button2: PropTypes.string,
   image_alt: PropTypes.string,
 }
 
